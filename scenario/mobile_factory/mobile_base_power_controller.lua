932e545519be60933e3693b94a26047817b526ec
马文凯 <909771070@qq.com>
2022-05-14 21:16:22 +0800

    基地能给基地蜘蛛充电

--- scenario/mobile_factory/mobile_base_power_controller.lua
+++ scenario/mobile_factory/mobile_base_power_controller.lua
@@ -17,29 +17,43 @@ function MobileBasePowerController:can_recharge_equipment_for_character()
     return base:can_run() and not base:is_heavy_damaged()
 end
 
-function MobileBasePowerController:recharge_equipment_for_character(character)
-    local grid = character and character.valid and character.grid
-    if not grid then return end
-    local base = self:get_base()
-    if base:can_run() and not base:is_heavy_damaged() then
-        local hyper_accumulator = base.hyper_accumulator
-        for _, equipment in pairs(grid.equipment) do
-            if equipment.energy < equipment.max_energy then
-                local require_energy = equipment.max_energy - equipment.energy
-                if hyper_accumulator.energy >= require_energy then
-                    equipment.energy = equipment.max_energy
-                    hyper_accumulator.energy = hyper_accumulator.energy - require_energy
-                else
-                    equipment.energy = equipment.energy + hyper_accumulator.energy
-                    hyper_accumulator.energy = 0
-                end
+local function recharge_equipment(energy_source, grid)
+    for _, equipment in pairs(grid.equipment) do
+        if equipment.energy < equipment.max_energy then
+            local require_energy = equipment.max_energy - equipment.energy
+            if energy_source.energy >= require_energy then
+                equipment.energy = equipment.max_energy
+                energy_source.energy = energy_source.energy - require_energy
+            else
+                equipment.energy = equipment.energy + energy_source.energy
+                energy_source.energy = 0
             end
         end
     end
 end
 
+function MobileBasePowerController:recharge_equipment_for_character(character)
+    local grid = character and character.valid and character.grid
+    if not grid then return end
+    local base = self:get_base()
+    if base:can_run() and not base:is_heavy_damaged() then
+        recharge_equipment(base.hyper_accumulator, grid)
+    end
+end
+
 function MobileBasePowerController:run()
     self:update_generators()
+    self:recharge_base_equipment()
+end
+
+function MobileBasePowerController:recharge_base_equipment()
+    local base = self:get_base()
+    if not base:is_heavy_damaged() then
+        local grid = base.vehicle.grid
+        if grid then
+            recharge_equipment(base.hyper_accumulator, grid)
+        end
+    end
 end
 
 function MobileBasePowerController:update_generators()
