250fe8b5c679490b8b3049aff113bf4cc4dc43bf
马文凯 <909771070@qq.com>
2022-06-05 16:47:30 +0800

    fix: command by console

--- klib/gmo/command.lua
+++ klib/gmo/command.lua
@@ -3,7 +3,7 @@ local Command = {}
 function Command.add_admin_command(name, help, func)
     commands.add_command(name, help, function(data)
         local player = game.get_player(data.player_index)
-        if not player.admin then
+        if player and not player.admin then
             player.print({"klib.require_admin_permission"})
             return
         end
