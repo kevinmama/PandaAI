e0a6dcb9f6a5ea16e1d26aa95a1bf8590008a0e0
马文凯 <909771070@qq.com>
2022-06-08 14:16:00 +0800

    call on player.character.die() will cause desync

--- control.lua
+++ control.lua
@@ -1,9 +1,9 @@
-if script.active_mods["gvv"] then require("__gvv__.gvv")() end
+--if script.active_mods["gvv"] then require("__gvv__.gvv")() end
 
 --require 'config'
 
 --require 'klib/container/container'
-require 'klib/klib'
+--require 'klib/klib'
 
 -- widges
 --require 'widget/debug_panel'
@@ -33,21 +33,21 @@ require 'klib/klib'
 --require 'klib/spec/spiral_index_spec'
 --require 'klib/spec/linked_list_spec'
 --require 'klib/spec/table_size_spec'
---require 'klib/spec/entity_die_desync'
+require 'klib/spec/entity_die_desync'
 
 --------------------------------------------------------------------------------
 --- ### Scenario ###
 --------------------------------------------------------------------------------
 --require 'scenario/quickstart/main'
 --require 'scenario/tow/solder_spawner_manager'
-require 'scenario/mobile_factory/main'
+--require 'scenario/mobile_factory/main'
 --require 'scenario/nauvis_war/main'
 
 --------------------------------------------------------------------------------
 --- ### For Debug Checking, Should Not Change Global in control stage ###
 --------------------------------------------------------------------------------
 
-if next(global) then
-    log("!!! detected changing global in control stage !!!")
-    log(serpent.line(global))
-end
+--if next(global) then
+--    log("!!! detected changing global in control stage !!!")
+--    log(serpent.line(global))
+--end
